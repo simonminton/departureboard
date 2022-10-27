6d65ae6a13d73d8b67ed39bf67a7c9df5a7c5065
Simon Minton <simon@minton.co>
2022-10-27 15:27:19 +0100

    test

--- src/components/OldDisplayBoard.vue
+++ src/components/OldDisplayBoard.vue
@@ -2,7 +2,84 @@
   <div class="w-full flex flex-row flex-wrap-reverse md:flex-wrap">
     <div class="w-full md:w-1/4 bg-gray-800 text-white p-4">
       <a href="/"> Home </a>
-      
+      <a class="py-1" :href="'/' + $route.params.station">
+        &rarr; {{ this.stationName }}
+      </a>
+      <a
+        v-if="$route.params.line"
+        class="py-1 uppercase"
+        :href="'/' + $route.params.station + '/' + $route.params.line"
+      >
+        &rarr; {{ $route.params.line }}
+      </a>
+      <a
+        v-if="$route.params.direction"
+        class="py-1"
+        :href="
+          '/' +
+          $route.params.station +
+          '/' +
+          $route.params.line +
+          '/' +
+          $route.params.direction
+        "
+      >
+        &rarr; {{ $route.params.direction }}
+      </a>
+      <div
+        v-if="lines"
+        class="text-white font-semibold border-b border-gray-600 my-2 pb-2 mt-4"
+      >
+        Lines
+      </div>
+      <div
+        v-for="(line, lindx) in lines"
+        v-bind:key="lindx"
+        class="text-white inline pr-2 pb-2 mt-4 text-sm font-semibold uppercase"
+      >
+        <a
+          :class="'bg-' + line.lineName"
+          class="px-2 py-1"
+          :href="'/' + $route.params.station + '/' + line.lineName"
+        >
+          {{ line.lineName }}
+        </a>
+      </div>
+      <div
+        v-if="$route.params.line"
+        class="text-white font-semibold border-b border-gray-600 pb-2 mt-4"
+      >
+        Options
+      </div>
+      <div v-if="$route.params.line" class="text-white pb-2 mt-4 flex flex-row">
+        <a
+          class="w-1/2 block text-center"
+          :href="
+            '/' + $route.params.station + '/' + $route.params.line + '/inbound'
+          "
+        >
+          Inbound
+        </a>
+        <a
+          class="w-1/2 block text-center"
+          :href="
+            '/' + $route.params.station + '/' + $route.params.line + '/outbound'
+          "
+        >
+          Outbound
+        </a>
+      </div>
+    </div>
+    <ul
+      v-if="trainData"
+      class="w-full md:w-3/4 rounded-md px-4 md:px-8 max-w-xl mx-auto dotmatrix"
+    >
+      <li v-for="(train, indx) in trainData" v-bind:key="indx">
+        <SingleTrain :train="train" />
+      </li>
+    </ul>
+    <div v-else>
+      <div class="text-white text-center">Loading data...</div>
     </div>
   </div>
 </template>
@@ -29,8 +106,8 @@ export default {
   },
   mounted() {
     // fetch data from the API every 0 seconds
-    // this.getTrainData();
-    // this.getStationLines();
+    this.getTrainData();
+    this.getStationLines();
   },
   methods: {
     getStationLines() {
